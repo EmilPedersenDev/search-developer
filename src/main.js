6daed21d0d8a86066866517e3838107a194e1c35
Emil Pedersen <emil.pedersen@stretch.se>
2020-11-17 14:40:10 +0100

    added vuelidate

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Vuelidate from "vuelidate";
 
 import "./common/index";
 import "./assets/scss/main.scss";
 
 Vue.config.productionTip = false;
+
+Vue.use(Vuelidate);
 
 new Vue({
   router,
